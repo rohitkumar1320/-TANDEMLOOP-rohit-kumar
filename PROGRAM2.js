91a45d59348c1c29928b654ef256a0099e2661b7
Rohit Kumar <165240156+rohitkumar1320@users.noreply.github.com>
2025-06-26 21:30:29 +0530

    Update PROGRAM2.js

--- PROGRAM2.js
+++ PROGRAM2.js
@@ -9,3 +9,7 @@ function oddSeries(x) {
 
 const input = 16;
 console.log("Output:", oddSeries(input));
+
+//THE OUTPUT FOR THIS//
+
+Output: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31
